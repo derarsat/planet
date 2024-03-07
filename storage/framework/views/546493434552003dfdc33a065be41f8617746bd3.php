7c161519e258d96bfdbeea6436d77ad6e1198437
Derar Sattouf <derar.satouf@toothpick.com>
2024-03-07 21:15:27 +0200

    test

---
+++ storage/framework/views/546493434552003dfdc33a065be41f8617746bd3.php
@@ -0,0 +1,74 @@
+<div class="bg-dark">
+    <div class="container">
+        <div class="py-12 lg:py-24">
+            <img src="<?php echo e(Vite::asset('resources/images/logo-white.webp')); ?>" alt="Planet Bottling"
+                 class="w-44">
+
+            <div class="grid grid-cols-1 lg:grid-cols-4 gap-6 text-white mt-12 items-start justify-start">
+                <div class="grid grid-cols-1 gap-2 font-light">
+                    <a href="" class="font-bold mb-6 ">Useful Links</a>
+                    <a href="">Home</a>
+                    <a href="">Who We Are</a>
+                    <a href="">Brands</a>
+                    <a href="">Global Presence</a>
+                    <a href="">Media</a>
+                    <a href="">Get In Touch</a>
+                </div>
+
+                <div class="grid grid-cols-1 gap-2 font-light">
+                    <a href="" class="font-bold mb-6 ">Brands</a>
+                    <a href="">Planet</a>
+                    <a href="">Original American Cola</a>
+                    <a href="">Reaktor</a>
+                    <a href="">Bubble Up</a>
+                </div>
+
+                <div class="grid grid-cols-1 gap-2 font-light">
+                    <a href="" class="font-bold mb-6 ">Get In Touch</a>
+                    <a href="">Feedback</a>
+                    <a href="">Marketing</a>
+                    <a href="">Sales</a>
+                </div>
+
+                <div class="grid grid-cols-1 gap-6 font-light">
+                    <a href="" class="font-bold mb-6 ">Contact Information</a>
+                    <span class="flex items-start gap-4">
+                        <svg class="flex-shrink-0" id="ic_location" data-name="ic location" xmlns="http://www.w3.org/2000/svg" width="13.263"
+                             height="21.566" viewBox="0 0 13.263 21.566">
+                            <path id="Path_1477" data-name="Path 1477"
+                                  d="M1308.405,267.266a11.185,11.185,0,0,1-4.665-.835c-1.2-.557-1.8-1.267-1.8-2.13a1.9,1.9,0,0,1,.661-1.375,4.791,4.791,0,0,1,1.819-1.052l.482,1.564a3.287,3.287,0,0,0-1.373.863,3.991,3.991,0,0,0,1.9.944,11,11,0,0,0,2.983.4,11.134,11.134,0,0,0,2.991-.4,3.946,3.946,0,0,0,1.912-.944,4.274,4.274,0,0,0-.561-.458,3.431,3.431,0,0,0-.812-.4l.46-1.564a4.847,4.847,0,0,1,1.819,1.052,1.923,1.923,0,0,1,.661,1.375c0,.863-.6,1.573-1.8,2.13A11.185,11.185,0,0,1,1308.405,267.266Zm0-2.857a.908.908,0,0,1-.568-.188,1.091,1.091,0,0,1-.352-.485,11.277,11.277,0,0,0-1.618-3.262q-.97-1.294-1.883-2.48a15.4,15.4,0,0,1-1.567-2.466,7.047,7.047,0,0,1-.647-3.195,6.632,6.632,0,1,1,13.263,0,7.047,7.047,0,0,1-.647,3.195,15.344,15.344,0,0,1-1.56,2.466c-.611.791-1.244,1.617-1.891,2.48a11.283,11.283,0,0,0-1.617,3.262,1.076,1.076,0,0,1-.345.485A.908.908,0,0,1,1308.405,264.409Zm0-10.46a1.6,1.6,0,1,0-1.15-.472A1.563,1.563,0,0,0,1308.405,253.949Z"
+                                  transform="translate(-1301.77 -245.7)" fill="#fff"/>
+                        </svg>
+
+                        <p>Corporate Office: 4th and 5th Floor Africa Re Building, Plot 1679 Karimu Kotun Street,
+                            Victoria Island, Lagos.</p>
+                    </span>
+
+                    <span class="flex items-start gap-4">
+                        <svg class="flex-shrink-0" id="ic_location" data-name="ic location" xmlns="http://www.w3.org/2000/svg" width="13.263"
+                             height="21.566" viewBox="0 0 13.263 21.566">
+                            <path id="Path_1477" data-name="Path 1477"
+                                  d="M1308.405,267.266a11.185,11.185,0,0,1-4.665-.835c-1.2-.557-1.8-1.267-1.8-2.13a1.9,1.9,0,0,1,.661-1.375,4.791,4.791,0,0,1,1.819-1.052l.482,1.564a3.287,3.287,0,0,0-1.373.863,3.991,3.991,0,0,0,1.9.944,11,11,0,0,0,2.983.4,11.134,11.134,0,0,0,2.991-.4,3.946,3.946,0,0,0,1.912-.944,4.274,4.274,0,0,0-.561-.458,3.431,3.431,0,0,0-.812-.4l.46-1.564a4.847,4.847,0,0,1,1.819,1.052,1.923,1.923,0,0,1,.661,1.375c0,.863-.6,1.573-1.8,2.13A11.185,11.185,0,0,1,1308.405,267.266Zm0-2.857a.908.908,0,0,1-.568-.188,1.091,1.091,0,0,1-.352-.485,11.277,11.277,0,0,0-1.618-3.262q-.97-1.294-1.883-2.48a15.4,15.4,0,0,1-1.567-2.466,7.047,7.047,0,0,1-.647-3.195,6.632,6.632,0,1,1,13.263,0,7.047,7.047,0,0,1-.647,3.195,15.344,15.344,0,0,1-1.56,2.466c-.611.791-1.244,1.617-1.891,2.48a11.283,11.283,0,0,0-1.617,3.262,1.076,1.076,0,0,1-.345.485A.908.908,0,0,1,1308.405,264.409Zm0-10.46a1.6,1.6,0,1,0-1.15-.472A1.563,1.563,0,0,0,1308.405,253.949Z"
+                                  transform="translate(-1301.77 -245.7)" fill="#fff"/>
+                        </svg>
+
+                        <p>
+                            Factory: KM 52, Aiyetoro Village, Lagos-Ibadan Expressway, Ogun State.VT
+                        </p>
+                    </span>
+                    <span class="flex items-center gap-4">
+                        <svg class="flex-shrink-0" id="ic_phone" data-name="ic phone" xmlns="http://www.w3.org/2000/svg" width="16.334" height="16.334" viewBox="0 0 16.334 16.334">
+                            <path id="Path_1479" data-name="Path 1479" d="M1312.812,327.534a12.223,12.223,0,0,1-5.5-1.361,18.876,18.876,0,0,1-8.451-8.451,12.233,12.233,0,0,1-1.361-5.5,1.01,1.01,0,0,1,1.022-1.021h3.176a.814.814,0,0,1,.557.215.993.993,0,0,1,.3.579l.617,2.858a1.661,1.661,0,0,1-.012.578.915.915,0,0,1-.242.442l-2.269,2.292a18.786,18.786,0,0,0,2.849,3.675,15.781,15.781,0,0,0,3.575,2.654l2.154-2.223a1.228,1.228,0,0,1,.52-.351,1.056,1.056,0,0,1,.593-.034l2.7.59a.964.964,0,0,1,.563.34.934.934,0,0,1,.23.635v3.063a1.009,1.009,0,0,1-1.022,1.021Z" transform="translate(-1297.5 -311.2)" fill="#fff"/>
+                        </svg>
+
+
+                        <p>
+                            +234 (0) 8123000000
+                        </p>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php /**PATH /Users/derarsattouf/Documents/code/personal/planet/resources/views/components/app-footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/7695d35aa3e14cf5fa9aa95bedbfdcfa8c8c445d.php
@@ -0,0 +1,27 @@
+
+<div class="text-center grid grid-cols-1 gap-12 pb-24">
+    <h2 class="text-primary font-bold text-4xl lg:text-7xl">OUR BRANDS</h2>
+    <p class="max-w-3xl px-4 mx-auto font-light text-text relative">
+        At Planet Bottling Company, we take pride in bringing you a diverse selection of affordable quality
+        beverages with a unique taste that cater to all. From refreshing carbonated soft drinks to functional
+        energy drinks, our range is thoughtfully crafted to provide you with a delightful beverage experience.
+        We source the finest ingredients to ensure each sip is filled with authentic flavors and utmost
+        satisfaction. <br> <br> With a strong commitment to quality and affordability, our beverages are the
+        perfect
+        choice for discerning consumers seeking exceptional taste at a reasonable price.
+        <img src="<?php echo e(Vite::asset('resources/images/bubble-1.webp')); ?>" alt="Planet Bottling Locations"
+             class="w-64 absolute -top-[8rem] -right-[8rem] hidden lg:block">
+    </p>
+    <div class="flex justify-center">
+        <button class="bg-primary text-white font-light px-6 py-2 rounded-full">Learn More</button>
+
+    </div>
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-12 lg:gap-24 container py-6 lg:py-0">
+        <?php $__currentLoopData = $brands; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $brand): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+
+            <img src="<?php echo e(@App::make('url')->to('/') . '/storage/' . $brand->greyscaleImage); ?>" class="w-full aspect-square"/>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </div>
+</div>
+
+<?php /**PATH /Users/derarsattouf/Documents/code/personal/planet/resources/views/components/brands.blade.php ENDPATH**/ ?>
