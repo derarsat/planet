ba172b58ac38e80cb6cc6d10dee4f5a18b159a04
Derar Sattouf <derar.satouf@toothpick.com>
2024-03-07 20:51:19 +0200

    test

--- storage/framework/views/64e46023e0bc6eeb02c3996337ddc3b02009eeff.php
+++
@@ -1,5 +0,0 @@
-
-
-
-
-<?php /**PATH /Users/derarsattouf/Documents/code/personal/planet/resources/views/vendor/backpack/base/inc/topbar_right_content.blade.php ENDPATH**/ ?>
--- storage/framework/views/7229cdc64f2bdfe30d859f8fdd3283db17b40378.php
+++
@@ -1,4 +0,0 @@
-<?php if(isset($entry->image)): ?>
-    <img src="<?php echo e(@App::make('url')->to('/') . '/storage/' . $entry->historyImage); ?>" width="120"/>
-<?php endif; ?>
-<?php /**PATH /Users/derarsattouf/Documents/code/personal/planet/resources/views/partials/historyImage.blade.php ENDPATH**/ ?>
