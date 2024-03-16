f49619318a9c244d4823f1cb3735f79029cfabd7
Derar Sattouf <derar.satouf@toothpick.com>
2024-03-16 02:44:46 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,6 @@ Route::get('/get-in-touch', function () {
 
 Route::get('/product/{id}', function () {
     $product = \App\Models\Product::with('productFlavors')->find(request()->id);
-    if(!$product)  abort(404);
+    if(!$product)  redirect('/');
     return view('product', compact('product'));
 });
