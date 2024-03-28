e1f270f8970657ddfa0855f5b412ff7cca02ecf2
Derar Sattouf <derar.satouf@toothpick.com>
2024-03-28 04:02:51 +0200

    test

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <title>Planet Bottling</title>
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="/slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="/slick/slick-theme.css"/>
