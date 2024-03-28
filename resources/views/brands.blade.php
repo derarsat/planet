3322925bbe1241fefd686b2b1d061920f753e8a4
Derar Sattouf <derar.satouf@toothpick.com>
2024-03-28 03:42:44 +0200

    test

--- resources/views/brands.blade.php
+++ resources/views/brands.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 
 @section('content')
-    <img src="/images/brands-banner.webp" alt="Planet Bottling Brands"/>
-
+{{--    <img src="/images/brands-banner.webp" alt="Planet Bottling Brands"/>--}}
+    <div class="min-h-[60vh] lg:min-h-[90vh] bg-left bg-cover" style="background-image: url('/images/brands-banner.webp')"> </div>
     <div class="container flex items-center justify-center gap-4 flex-wrap mt-8">
         <button class="cat px-4 py-2 rounded-full bg-primary text-white">All Products</button>
         @foreach($brands as $brand)
@@ -10,10 +10,10 @@
             <button class="px-4 py-2 rounded-full cat ">{{$brand->category}}</button>
         @endforeach
     </div>
-    <div class="container py-12 grid grid-cols-1 gap-12 text-dark">
+    <div class="max-w-7xl mx-auto py-12 grid grid-cols-1 gap-4 lg:gap-12 text-dark px-2 lg:px-0">
         @foreach($brands as $brand)
             <div data-category="{{$brand->category}}"
-                 class="dis-cat bg-grey rounded-2xl p-6 lg:p-12 grid grid-cols-3 gap-12 lg:gap-24">
+                 class="dis-cat bg-grey rounded-2xl flex items-center flex-wrap gap-8 p-4 lg:p-8">
                 <div class="order-2 lg:order-1 flex col-span-3 lg:col-span-2 flex-col items-start justify-between">
                     <div class="flex flex-col gap-4">
                         <h1 class="text-3xl lg:text-5xl font-semibold">
@@ -25,11 +25,11 @@
                     </div>
                     @if(count($brand->products) > 0)
                         <a href="/product/{{$brand->products[0]->id}}"
-                           class="bg-primary text-white px-4 py-2 rounded-full mt-6 lg:mt-0">Learn More </a>
+                           class="bg-primary text-white px-4 py-2 rounded-full mt-6">Learn More </a>
 
                     @endif
                 </div>
-                <div class=" aspect-[462/285] group relative rounded-3xl overflow-hidden">
+                <div class="aspect-[462/285] w-full flex-shrink-0 lg:w-96 group relative rounded-3xl overflow-hidden">
                     <img src="{{@App::make('url')->to('/') . '/storage/' . $brand->hero  }}"
                          class="absolute inset-0 h-full w-full opacity-100 aspect-square transition duration-1000 group-hover:opacity-0"/>
                     <img src="{{@App::make('url')->to('/') . '/storage/' . $brand->greyscaleHero  }}"
