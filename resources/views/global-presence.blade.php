51a44942df3c08b6371f2f4b9c9ed9a095677731
Derar Sattouf <derar.satouf@toothpick.com>
2024-06-05 19:40:30 +0300

    test

--- resources/views/global-presence.blade.php
+++ resources/views/global-presence.blade.php
@@ -3,7 +3,7 @@
     <div class="relative h-[85vh] bg-center bg-cover"
          style="background-image: url('images/global-presence-hero.webp')">
         <div class="absolute z-10 flex items-center justify-center inset-0 bg-black bg-opacity-30">
-            <h1 class="text-white font-semibold text-6xl text-center uppercase uppercase">
+            <h1 class="text-white font-semibold text-4xl lg:text-6xl text-center uppercase uppercase">
                 mastering bottling <br> for global distribution
             </h1>
         </div>
